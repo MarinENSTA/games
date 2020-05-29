b50de4053ba3401f7d0f71926fc61973b8c31b27
MarinENSTA <62882158+MarinENSTA@users.noreply.github.com>
2020-05-30 00:01:51 +0200

    Final commit for this project !
    Lots about pygame learnt during this 1-day project

--- snake python/snake_game.py
+++ snake python/snake_game.py
@@ -1,1 +1,84 @@
 #Project's main
+
+import pygame
+from pygame.locals import *
+
+import snake_module as sm
+from random import choice
+
+pygame.init()
+
+#Ouverture de la fenêtre Pygame (carré : largeur = hauteur)
+fenetre = pygame.display.set_mode((750,750))
+#Icone
+icone = pygame.image.load("images/snake_icon.png")
+pygame.display.set_icon(icone)
+#Titre
+pygame.display.set_caption("Snake")
+
+background=pygame.image.load('images/background_square.png').convert()
+body = pygame.image.load('images/snake_square.png').convert()
+fruit=pygame.image.load('images/red_square.png').convert()
+dead=pygame.image.load('images/game_over.jpg').convert()
+victory=pygame.image.load('images/victory.png').convert()
+
+squaresize=30
+
+snake=sm.Snake()
+
+fenetre.blit(background,(0,0))
+snake.init_fruit()
+snake.display(fenetre,body,fruit)
+
+
+pygame.display.flip()
+continuer=1
+while continuer==1:
+    
+    if snake.biteself==True:
+        pygame.time.wait(50)
+        fenetre.blit(dead, (0,0))
+        pygame.display.flip()
+        continuer=0
+    elif len(snake.tail)==625:
+        pygame.time.wait(50)
+        fenetre.blit(victory, (0,0))
+        pygame.display.flip()
+        continuer=0
+    else:
+        pygame.time.Clock().tick(30)
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                continuer=0
+                
+            elif event.type == KEYDOWN:
+                if event.key == K_d:
+                    success=snake.new_dir('droite')
+                elif event.key == K_a:
+                    success=snake.new_dir('gauche')
+                elif event.key == K_w:
+                    success=snake.new_dir('haut')
+                elif event.key == K_s:
+                    success=snake.new_dir('bas')
+                
+                if success:
+                    fenetre.blit(background,(0,0))
+                    snake.display(fenetre,body,fruit)
+                    pygame.display.flip()
+                    pygame.time.wait(100)
+            
+            else:
+                success=False
+    
+        if not success:
+            snake.auto_dir()
+            fenetre.blit(background,(0,0))
+            snake.display(fenetre,body,fruit)
+            pygame.display.flip()
+            pygame.time.wait(100)
+    
+    
+
+
+    
+
--- snake python/snake_module.py
+++ snake python/snake_module.py
@@ -1,1 +1,114 @@
 #Classes and methods related to snake game
+import pygame
+from random import choice
+from pygame.locals import *
+
+squaresize=30
+
+class Snake:
+    
+    def __init__(self):
+        # directions : g d h b
+        self.dir='droite'
+        self.biteself=False
+        #level=self.level
+        
+        #position
+        self.x = 3
+        self.z = 0
+        self.tail=[[3,0],[2,0],[1,0],[0,0]]
+        self.length=4
+        
+        self.fruit_x=0
+        self.fruit_z=0
+        self.fruit_pos_x=0
+        self.fruit_pos_z=0
+    
+    def auto_dir(self):
+        if self.dir == 'bas' :
+            self.z+=1
+        if self.dir == 'gauche' :
+            self.x-=1
+        if self.dir == 'haut' :
+            self.z-=1
+        if self.dir == 'droite' :
+            self.x+=1
+        self.move()
+
+    def new_dir(self,direction):
+        success=False
+        
+        if direction == 'bas' and self.dir != 'haut' :
+            self.z+=1
+            self.dir=direction
+            success=True
+                
+        elif direction == 'gauche' and self.dir != 'droite' :
+            self.x-=1
+            self.dir=direction
+            success=True
+            
+        elif direction == 'haut' and self.dir != 'bas' :
+            self.z-=1
+            self.dir=direction
+            success=True
+                
+        elif direction == 'droite' and self.dir != 'gauche' :
+            self.x+=1
+            self.dir=direction
+            success=True
+        
+        if success==True:
+            self.move()
+        
+        return success
+        
+        
+        #print('x : ',self.x)
+        #print('z : ',self.z)
+
+    def move(self):
+        if self.z==25:
+            self.z=0
+        elif self.x==-1:
+            self.x=24
+        elif self.z==-1:
+            self.z=24
+        elif self.x==25:
+            self.x=0
+        
+        self.tail.insert(0,[self.x,self.z])
+        
+        if self.tail[0]==[self.fruit_x,self.fruit_z]:
+            self.init_fruit()
+        elif self.tail[0] in self.tail[1:]:
+            self.biteself=True
+        else:
+            del self.tail[-1]
+        
+        
+    
+    def display(self,fenetre,body,fruit):
+        for square in self.tail:
+            temp_x=square[0]*squaresize
+            temp_z=square[1]*squaresize
+            fenetre.blit(body, (temp_x,temp_z))
+        fenetre.blit(fruit, (self.fruit_x*squaresize,self.fruit_z*squaresize))
+            
+    def init_fruit(self):
+        Sequence=[i for i in range(25)]
+        self.fruit_x=choice(Sequence)
+        self.fruit_z=choice(Sequence)
+        
+        while [self.fruit_x,self.fruit_z] in self.tail:
+            self.fruit_x=choice(Sequence)
+            self.fruit_z=choice(Sequence)
+        
+
+    
+
+            
+            
+        
+        
+        
